8e921f067f76f7e6c32b77f1f7b22aa0f0645830
Aiden <aidenspotify0000@gmail.com>
2026-08-07 17:58:29 +0330

    removed defualt tests of jamine and wrote money.js tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,19 @@
+import {formatCurency} from '../scripts/utils/money.js';
+
+describe('test Suite: formatCurrency', () => {
+  it('Converts Cents Into Dollars', () => {
+    expect(formatCurency(2095)).toEqual('20.95');
+  });
+
+  it('works with zero', () => {
+    expect(formatCurency(0)).toEqual('0.00');
+  });
+
+  it('2000.5 = 20.01', () =>{
+    expect(formatCurency(2000.5)).toEqual('20.01');
+  });
+
+  it('2000.4 = 20.00', () => {
+    expect(formatCurency(2000.4)).toEqual('20.00');
+  });
+});
