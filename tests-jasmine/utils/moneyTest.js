e1413c757c206ffb83e321f90e8eb05dd544c564
Aiden <aidenspotify0000@gmail.com>
2026-08-08 12:09:21 +0330

    dunno i write cart tests but they work for now

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurency} from '../scripts/utils/money.js';
+import {formatCurency} from '../../scripts/utils/money.js';
 
 describe('test Suite: formatCurrency', () => {
   it('Converts Cents Into Dollars', () => {
