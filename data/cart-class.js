a72473a9e86d625efe274249259be9404a490055
Aiden <aidenspotify0000@gmail.com>
2026-08-09 17:20:29 +0330

    made localstoragekey and loadfrom from storage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,15 +2,15 @@
 
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)); 
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)); 
 
     if (!this.cartItems) {
       this.cartItems = [{
@@ -26,7 +26,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
 
@@ -156,6 +156,7 @@ class Cart {
 
 const cart = new Cart('cart');
 const businessCart = new Cart('cart-business');
+
 
 
 console.log(cart);
