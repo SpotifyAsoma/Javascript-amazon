1f234ea5f436ee38eef3e34d2c1c02016894bac6
Aiden <aidenspotify0000@gmail.com>
2026-08-10 12:10:01 +0330

    warranty image add

--- data/products.js
+++ data/products.js
@@ -56,6 +56,25 @@ class Clothing extends Product {
   }
 }
 
+class Digital extends Product{
+  instructionsLink;
+  warrantyLink;
+
+  constructor (productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.instructionsLink}" target="_blank" >Instructions</a>
+    <a href="${this.warrantyLink}" target="_blank" >Warranty</a>
+    `;
+  }
+
+}
+
 
 
 export const products = [
@@ -118,7 +137,11 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'digital',
+    instructionsLink : 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png',
+    
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -305,7 +328,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: 'digital',
+    instructionsLink : 'images/no-image-:)',
+    warrantyLink: 'images/appliance-warranty.png',
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -610,7 +636,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'digital',
+    instructionsLink : 'images/no-image-:)',
+    warrantyLink: 'images/appliance-warranty.png',
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -670,7 +699,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'digital',
+    instructionsLink : 'images/no-image-:)',
+    warrantyLink: 'images/appliance-warranty.png',
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -722,7 +754,11 @@ export const products = [
 ].map((productDetails) => {
   if (productDetails.type === 'clothing') {
     return new Clothing(productDetails);
-  };
+  } else if (productDetails.type === 'digital') {
+    return new Digital(productDetails);
+  }
   return new Product(productDetails);
 });
+
+
 
