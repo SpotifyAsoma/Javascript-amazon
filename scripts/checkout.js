dde0a9832d3b6dc30230222993eea7ddad753be9
Aiden <aidenspotify0000@gmail.com>
2026-08-10 13:25:47 +0330

    added supersimple dev backend for practice

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import {renderOrderSummary} from './Checkout/orderSummary.js';
 import { renderPaymentSummary } from '../scripts/Checkout/paymentSummary.js';
 import { renderHeaderHTML } from './Checkout/checkoutHeader.js';
 //import '../data/cart-class.js';
+import '../data/backend-practice.js' 
 
 
 renderHeaderHTML();
